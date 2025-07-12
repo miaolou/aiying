511d2f6da4b357440195837b4b40ca92df5141cf
Mcdull <miaolou@gmail.com>
2025-07-12 15:01:16 +0800

    Add files via upload

---
+++ os/Centos_install_x86_64.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "开始安装爱影CMS最新版本"
+rm -f iycms.zip
+wget --no-check-certificate "https://www.iycms.com/api/v1/download/cms/latest?os=1&kind=x86_64" -O iycms.zip
+echo "解压文件"
+unzip -o -q iycms.zip -d /home/iycms
+rm -f iycms.zip
+echo "开始安装系统服务"
+
+echo "[Unit]
+Description=iycms service
+Documentation=https://www.iycms.com
+
+[Service]
+Type=simple
+Restart=always
+User=root
+WorkingDirectory=/home/iycms/
+ExecStart=/home/iycms/cms
+RestartSec=1
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=iycms
+
+[Install]
+WantedBy=multi-user.target">/etc/systemd/system/iycms.service
+echo 'if $programname == "iycms" then /home/iycms/stdout.log
+& stop'>/etc/rsyslog.d/iycms.conf
+touch /home/iycms/stdout.log
+chcon -t var_log_t /home/iycms/stdout.log
+systemctl daemon-reload
+systemctl restart rsyslog
+systemctl enable iycms
+systemctl restart iycms
+rm -f ./iycms.sh
+systemctl stop firewalld
+systemctl disable firewalld
+echo "安装完成"
